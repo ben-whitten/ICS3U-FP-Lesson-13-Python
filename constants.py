a39b96facf585f1144a023a6a5148c4d9699426b
ben-whitten <54954680+ben-whitten@users.noreply.github.com>
2019-12-11 13:10:54 -0500

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Created by: Ben Whitten
+# Created on: December 2019
+# Constants for FP
+
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 4
+ATTACK_SPEED = 10
+ENEMY_SPEED = 2
+TOTAL_ATTACKS = 20
+TOTAL_ENEMIES = 10
+OFF_SCREEN_X = -100
+OFF_SCREEN_Y = -100
+OFF_TOP_SCREEN = -1 * SPRITE_SIZE
+OFF_BOTTOM_SCREEN = SCREEN_Y + SPRITE_SIZE
+
+NEW_PALETTE = (b'\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
+
+button_state = {
+    "button_up": "up",
+    "button_just_pressed": "just pressed",
+    "button_still_pressed": "still pressed",
+    "button_released": "released"
+}
